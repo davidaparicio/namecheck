faa414fa11f9145afd10472e8f05e14abde707f5
David Aparicio <david.aparicio@free.fr>
2022-07-25 16:51:41 +0200

    📚 docs: add Go SAST / static-analysis#go

--- run_benchmark_only.sh
+++ run_benchmark_only.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 
-FuzzFUNC="Fuzz" #"FuzzReverse"
-
-echo "Let's Test"
-go test -v ./... -coverprofile=coverage.out
-
-echo "Let's Fuzz"
-go test -fuzz ${FuzzFUNC} -fuzztime 15s
-
 echo "Let's Bench"
-go test -v -run=^$ -bench . -benchmem -benchtime=3s ./
+go test -v -run=^$ -bench . -benchmem -benchtime=15s ./
